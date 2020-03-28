d672eff61edff57aaec2538769d3cf47e48adca9
anishkasachdeva <anishkasachdeva25@gmail.com>
2020-03-28 19:18:25 +0530

    changed the mate function with probabilities

--- script.py
+++ script.py
@@ -14,7 +14,7 @@ class model(object):
     # def __hash__(self):
     #     return hash(str(self.model))
     def cal_fitness(self):
-        fitness = (self.err[0] + self.err[1]) + abs(self.err[0]-self.err[1])
+        fitness = (self.err[0] + self.err[1]) + abs(self.err[0] - self.err[1])
         return fitness
 
 # def compare_models(model1,model2):
@@ -23,8 +23,8 @@ class model(object):
 #             return False
 #     return True
 
-f = open('test2.txt','r')
-f2=open('test.txt','w')
+f = open('test.txt','r')
+f2=open('test2.txt','w')
 message = f.read()
 lines=message.split('\n')
 lines=lines[0:len(lines)-1]
@@ -41,17 +41,30 @@ for line in lines:
     for i in err_string:
         err_numbers.append(float(i))
     models.append(model(model_numbers,err_numbers))
-for i in models:
-    if i.err[0]<=10000000 or i.err[1]<=10000000:
-        f2.write(str(i.model))
-        f2.write(" ")
-        f2.write(str(i.err))
-        f2.write("\n")
-f2.close()
+
+
+# ttt=0
+# for i in models:
+#     ttt=1
+#     for j in i.model:
+#         if j>10 or j< -10:
+#             ttt=0
+#     if i.err[0]<=10000000 and i.err[1]<=10000000 and ttt==1:
+#         f2.write(str(i.model))
+#         f2.write(" ")
+#         f2.write(str(i.err))
+#         f2.write("\n")
+# f2.close()
+
+
 models = sorted(models, key = lambda x:x.fitness)
+
+
 # models=set(models)
 # models=list(models)
 # print(len(models))
+
+
 it=0
 count=0
 distinct_fitness_index=[]
@@ -65,6 +78,8 @@ while it<(len(models)-1):
 if models[len(models)-2].fitness!=models[len(models)-1].fitness :
     distinct_fitness_index.append(len(models)-1)
     count=count+1
+
+
 # print(count)
 # probablity_array=[1,2,3,4]
 # population=[1,2,3,4]
@@ -72,18 +87,31 @@ if models[len(models)-2].fitness!=models[len(models)-1].fitness :
 # parents = random.choices(population,weights=probablity_array,cum_weights=None,k=100)
 # print(parents)
 
-# for j in range(100):
-#     print(j+1,end=' ')
-#     print(distinct_fitness_index[j],end=' ')
-#     print(models[distinct_fitness_index[j]].err ,end=' ')
-#     print(models[distinct_fitness_index[j]].fitness)
+# ttt=0
+for i in distinct_fitness_index:
+    # ttt=1
+    # for j in i.model:
+    #     if j>10 or j< -10:
+    #         ttt=0
+    if models[i].fitness<10000000:
+        f2.write(str(models[i].model))
+        f2.write(" ")
+        f2.write(str(models[i].err))
+        f2.write("\n")
+f2.close()
+
+for j in range(100):
+    print(j+1,end=' ')
+    print(distinct_fitness_index[j],end=' ')
+    print(models[distinct_fitness_index[j]].err ,end=' ')
+    print(models[distinct_fitness_index[j]].fitness)
 
 
-print("[",end='')
-for j in range(35):
-    print(models[distinct_fitness_index[j]].model ,end=' ')
-    print(",",end='')
-print("]")
+# print("[",end='')
+# for j in range(35):
+#     print(models[distinct_fitness_index[j]].model ,end=' ')
+#     print(",",end='')
+# print("]")
 
 
 f.close()
